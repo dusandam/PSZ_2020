9033e243b1b6a4614f3940f01d93a8ace80c496e
dusandam <dusandam.995@gmail.com>
2020-09-20 18:59:59 +0200

    refactor, improve

--- linear_regression.py
+++ linear_regression.py
@@ -4,29 +4,21 @@ import numpy as np
 class LinearRegression:
 
     def __init__(self, learning_rate=0.001, n_iters=1000):
-        self.lr=learning_rate
-        self.n_iters=n_iters
-        self.weights=None
-        self.bias=None
+        self.rate=learning_rate
+        self.iterations=n_iters
+        self.w=None
+        self.w0=None
 
     def fit(self, X, y):
-        n_samples, n_features=X.shape
+        m, num=X.shape
+        self.w=np.zeros(num)
+        self.w0=0
 
-        # init parameters
-        self.weights=np.zeros(n_features)
-        self.bias=0
-
-        # gradient descent
-        for _ in range(self.n_iters):
-            y_predicted=np.dot(X, self.weights) + self.bias
-            # compute gradients
-            dw=(1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db=(1 / n_samples) * np.sum(y_predicted - y)
-
-            # update parameters
-            self.weights-=self.lr * dw
-            self.bias-=self.lr * db
+        for _ in range(self.iterations):
+            hipothesis=np.dot(X, self.w) + self.w0
+            self.w-=self.rate * (1 / m) * np.dot(X.T, (hipothesis - y))
+            self.w0-=self.rate * (1 / m) * np.sum(hipothesis - y)
 
     def predict(self, X):
-        y_approximated=np.dot(X, self.weights) + self.bias
-        return y_approximated
+        predicted=np.dot(X, self.w) + self.w0
+        return predicted
